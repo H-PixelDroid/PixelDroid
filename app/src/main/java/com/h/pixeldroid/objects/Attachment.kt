8de10b5884fcab5a7836132a01ce958019806942
Matthieu <61561059+Wv5twkFEKh54vo4tta9yu7dHa3@users.noreply.github.com>
2020-03-02 19:21:58 +0100

    Add Attachment

--- app/src/main/java/com/h/pixeldroid/objects/Attachment.kt
+++ app/src/main/java/com/h/pixeldroid/objects/Attachment.kt
@@ -1,4 +1,19 @@
 package com.h.pixeldroid.objects
 
-class Attachment {
+data class Attachment(
+    //Required attributes
+    val id: String,
+    val type: AttachmentType,
+    val url: String, //URL
+    val preview_url: String, //URL
+    //Optional attributes
+    val remote_url: String? = null, //URL
+    val text_url: String? = null, //URL
+    //TODO meta
+    val description: String? = null,
+    val blurhash: String? = null
+)
+
+enum class AttachmentType {
+
 }
