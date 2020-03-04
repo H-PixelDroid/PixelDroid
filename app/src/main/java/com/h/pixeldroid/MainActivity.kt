3f508b3a2727d3ff98713146f3ca7ae57b19eb28
mjaillot <marie.jaillot@epfl.ch>
2020-03-04 19:32:41 +0100

    First draft of the profile page

--- app/src/main/java/com/h/pixeldroid/MainActivity.kt
+++ app/src/main/java/com/h/pixeldroid/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.h.pixeldroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import com.h.pixeldroid.api.PixelfedAPI
 import com.h.pixeldroid.objects.Status
 import retrofit2.Call
@@ -17,6 +20,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener((View.OnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent) }))
+
+    }
 }
