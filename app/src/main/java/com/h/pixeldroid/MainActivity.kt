c4b805703d0bac4cbc04935906fb00c91aa34550
Matthieu <61561059+Wv5twkFEKh54vo4tta9yu7dHa3@users.noreply.github.com>
2020-03-04 15:19:45 +0100

    Remove example API usage in MainActivity

--- app/src/main/java/com/h/pixeldroid/MainActivity.kt
+++ app/src/main/java/com/h/pixeldroid/MainActivity.kt
@@ -14,33 +14,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
-    private var statuses: ArrayList<Status>? = null
-    private val BASE_URL = "https://pixelfed.de/"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loadData()
-    }
-
-
-    private fun loadData() {
-
-        val pixelfedAPI = PixelfedAPI.create(BASE_URL)
-
-        pixelfedAPI.timelinePublic(null, null, null, null, null)
-            .enqueue(object : Callback<List<Status>> {
-                override fun onResponse(call: Call<List<Status>>, response: Response<List<Status>>) {
-                    if (response.code() == 200) {
-                        Log.e("OK", response.body().toString())
-                        statuses = response.body() as ArrayList<Status>?
-                    }
-                }
-
-                override fun onFailure(call: Call<List<Status>>, t: Throwable) {
-                    Log.e("Ouch, not OK", t.toString())
-                }
-            })
     }
 
 }
