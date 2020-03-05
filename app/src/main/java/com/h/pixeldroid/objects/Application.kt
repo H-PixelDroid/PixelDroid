6ec486bc68bc317eea7a8f9211e5e422222e1dfb
Andrew Dobis <andrew.dobis@epfl.ch>
2020-03-05 20:37:01 +0100

    WIP posts

--- app/src/main/java/com/h/pixeldroid/objects/Application.kt
+++ app/src/main/java/com/h/pixeldroid/objects/Application.kt
@@ -1,9 +1,12 @@
 package com.h.pixeldroid.objects
 
+import java.io.Serializable
+
 data class Application (
     //Required attributes
     val name: String,
     //Optional attributes
     val website: String? = null,
     val vapid_key: String? = null
-)
+) : Serializable
+
