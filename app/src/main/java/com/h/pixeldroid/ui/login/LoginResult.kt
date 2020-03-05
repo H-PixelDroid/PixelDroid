85e1db49cac5a2c457637b43250d29eee752715d
Andrea Clement <samuel.dietz@epfl.ch>
2020-03-05 18:59:46 +0100

    deleted login template

--- app/src/main/java/com/h/pixeldroid/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.h.pixeldroid.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
