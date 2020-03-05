7713463a6dbaa279313363480699a75f2814f39f
Ulysse Widmer <ulysse.widmer@epfl.ch>
2020-03-05 15:25:06 +0900

    updt de la main activity

--- app/src/main/java/com/h/pixeldroid/MainActivity.kt
+++ app/src/main/java/com/h/pixeldroid/MainActivity.kt
@@ -2,12 +2,19 @@ package com.h.pixeldroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homepage_button : ImageButton = findViewById(R.id.activity_main_home_btn)
+        val search_button : ImageButton = findViewById(R.id.activity_main_search_btn)
+        val camera_button : ImageButton = findViewById(R.id.activity_main_camera_btn)
+        val favorite_button : ImageButton = findViewById(R.id.activity_main_favorite_btn)
+        val account_button : ImageButton = findViewById(R.id.activity_main_account_btn)
     }
 
 }
