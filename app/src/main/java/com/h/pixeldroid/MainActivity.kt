9daad1c0b5973a83890f0637cd5b5168be9f0c62
Andrew Dobis <andrew.dobis@epfl.ch>
2020-03-06 17:08:50 +0100

    rebased from master

--- app/src/main/java/com/h/pixeldroid/MainActivity.kt
+++ app/src/main/java/com/h/pixeldroid/MainActivity.kt
@@ -5,21 +5,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import com.h.pixeldroid.api.PixelfedAPI
-import com.h.pixeldroid.objects.Post
-import com.h.pixeldroid.objects.Status
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
-import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
 
