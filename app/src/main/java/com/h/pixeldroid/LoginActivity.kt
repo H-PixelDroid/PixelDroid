33f0711b1f0fad91241100b8ee05ca82f8cb00ca
Matthieu <61561059+Wv5twkFEKh54vo4tta9yu7dHa3@users.noreply.github.com>
2020-03-06 18:24:20 +0100

    Finish authentication

--- app/src/main/java/com/h/pixeldroid/LoginActivity.kt
+++ app/src/main/java/com/h/pixeldroid/LoginActivity.kt
@@ -2,88 +2,182 @@ package com.h.pixeldroid
 
 import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
 import com.h.pixeldroid.api.PixelfedAPI
 import com.h.pixeldroid.objects.Application
+import com.h.pixeldroid.objects.Token
 import kotlinx.android.synthetic.main.activity_login.*
+import okhttp3.HttpUrl
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
 
+    private val OAUTH_SCHEME = "oauth2redirect"
+    private val PACKAGE_ID = "com.h.pixeldroid"
+    private val SCOPE = "read write follow"
+    private val APP_NAME = "PixelDroid"
     private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        connect_instance_button.setOnClickListener { onClickConnect() }
+
+        preferences = getSharedPreferences(
+            "$PACKAGE_ID.pref", Context.MODE_PRIVATE
+        )
+
+
     }
 
-    fun connexion(view: View) {
+    override fun onStart(){
+        super.onStart()
+
+        val url = intent.data
+
+        if (url != null && url.toString().startsWith("$OAUTH_SCHEME://$PACKAGE_ID")) {
+
+            val code = url.getQueryParameter("code")
+            val error = url.getQueryParameter("error")
+
+            // Restore previous values from preferences
+            val domain = preferences.getString("domain", "")
+            val clientId = preferences.getString("clientID", "")
+            val clientSecret = preferences.getString("clientSecret", "")
+
+            if (code != null && !domain.isNullOrEmpty() && !clientId.isNullOrEmpty() && !clientSecret.isNullOrEmpty()) {
+                //Successful authorization
+                val callback = object : Callback<Token> {
+                    override fun onResponse(call: Call<Token>, response: Response<Token>) {
+                        if (response.isSuccessful) {
+                            authenticationSuccessful(domain, response.body()?.access_token)
+                        } else {
+                            failedRegistration("Error getting token")
+                        }
+                    }
+
+                    override fun onFailure(call: Call<Token>, t: Throwable) {
+                        failedRegistration("Error getting token")
+                    }
+                }
+
+                val pixelfedAPI = PixelfedAPI.create("https://$domain")
+                pixelfedAPI.obtainToken(clientId, clientSecret, "$OAUTH_SCHEME://$PACKAGE_ID", SCOPE, code,
+                    "authorization_code").enqueue(callback)
+            } else if (error != null) {
+                failedRegistration("Authentication denied")
+            } else {
+                failedRegistration("Unknown response")
+            }
+        }
+    }
+
+    private fun authenticationSuccessful(domain: String, accessToken: String?) {
+
+        Log.e("Token", accessToken!!)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun onClickConnect() {
+
+        connect_instance_button.isEnabled = false
+
+        val domain = editText.text.toString()
+
+        val normalizedDomain = normalizeDomain(domain)
+        try{
+            HttpUrl.Builder().host(domain).scheme("https").build()
+        } catch (e: IllegalArgumentException) {
+            failedRegistration("Invalid domain")
+            return
+        }
+
+        preferences.edit()
+            .putString("domain", normalizedDomain)
+            .apply()
 
         val callback = object : Callback<Application> {
             override fun onResponse(call: Call<Application>, response: Response<Application>) {
                 if (!response.isSuccessful) {
-                    // TODO
+                    failedRegistration()
+                    return
                 }
 
                 val credentials = response.body()
-                val clientId = credentials!!.client_id
+                val clientId = credentials?.client_id ?: return failedRegistration()
                 val clientSecret = credentials.client_secret
-                val domain = editText.text.toString()
 
-                preferences = getSharedPreferences(
-                    "com.h.PixelDroid.pref", Context.MODE_PRIVATE
-                )
-                preferences.edit().putString("clientID", clientId)
+
+                preferences.edit()
+                    .putString("clientID", clientId)
                     .putString("clientSecret", clientSecret)
-                    .putString("domain", domain)
                     .apply()
 
-                if (clientId != null) {
-                    redirect(domain, clientId)
-                }
+                redirect(normalizedDomain, clientId)
             }
 
             override fun onFailure(call: Call<Application>, t: Throwable) {
+                failedRegistration()
+                return
             }
         }
 
-        val api = PixelfedAPI.create("https://pixelfed.de")
-        api.registerApplication(
-                "pixeldroid",
-                "oauth2redirect://com.h.pixeldroid", "read write follow"
+        val pixelfedAPI = PixelfedAPI.create("https://$normalizedDomain")
+        pixelfedAPI.registerApplication(
+                APP_NAME,
+                "$OAUTH_SCHEME://$PACKAGE_ID", SCOPE
             )
             .enqueue(callback)
 
     }
 
-    fun redirect(domain: String, client_id: String) {
+    private fun failedRegistration(message: String =
+                                       "Could not register the application with this server"){
+        connect_instance_button.isEnabled = true
+        editText.error = message
+    }
+    private fun normalizeDomain(domain: String): String {
+        var d = domain.replace("http://", "")
+        d = d.replace("https://", "")
+        return d.trim(Char::isWhitespace)
+    }
 
-        val url = "https://" + domain + "/oauth/authorize" + "?" +
+    fun redirect(normalizedDomain: String, client_id: String) {
+
+        val url = "https://$normalizedDomain/oauth/authorize?" +
                 "client_id" + "=" + client_id + "&" +
-                "redirect_uri" + "=" + "oauth2redirect://com.h.pixeldroid" + "&" +
+                "redirect_uri" + "=" + "$OAUTH_SCHEME://$PACKAGE_ID" + "&" +
                 "response_type=code" + "&" +
-                "scope=read write follow"
+                "scope=$SCOPE"
 
         browser(this, url)
     }
 
-    fun browser(context: Context, url: String) {
+    private fun browser(context: Context, url: String) {
         val intent = CustomTabsIntent.Builder().build()
-
 
         try {
             intent.launchUrl(context, Uri.parse(url))
         } catch (e: ActivityNotFoundException) {
-            Log.w("login", "Could not launch browser")
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            if (browserIntent.resolveActivity(packageManager) != null) {
+                startActivity(browserIntent)
+            } else {
+                failedRegistration(message="Could not launch a browser, do you have one?")
+                return
+            }
         }
+        connect_instance_button.isEnabled = true
     }
 }
--- app/src/main/java/com/h/pixeldroid/api/PixelfedAPI.kt
+++ app/src/main/java/com/h/pixeldroid/api/PixelfedAPI.kt
@@ -2,6 +2,7 @@ package com.h.pixeldroid.api
 
 import com.h.pixeldroid.objects.Application
 import com.h.pixeldroid.objects.Status
+import com.h.pixeldroid.objects.Token
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -31,7 +32,18 @@ interface PixelfedAPI {
         @Field("website") website: String? = null
     ): Call<Application>
 
-    @GET("/api/v1/timelines/public")
+    @FormUrlEncoded
+    @POST("/oauth/token")
+    fun obtainToken(
+        @Field("client_id") client_id: String,
+        @Field("client_secret") client_secret: String,
+        @Field("redirect_uri") redirect_uri: String,
+        @Field("scope") scope: String? = "read",
+        @Field("code") code: String? = null,
+        @Field("grant_type") grant_type: String? = null
+    ): Call<Token>
+
+            @GET("/api/v1/timelines/public")
     fun timelinePublic(
         @Query("local") local: Boolean? = null,
         @Query("max_id") max_id: String? = null,
