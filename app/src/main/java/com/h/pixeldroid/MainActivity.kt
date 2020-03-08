1d13b5e6211a6b7e864582aed632fc79a538ba0c
Matthieu <61561059+Wv5twkFEKh54vo4tta9yu7dHa3@users.noreply.github.com>
2020-03-08 12:54:18 +0100

    Move settings to activity, remove upper bar

--- app/src/main/java/com/h/pixeldroid/MainActivity.kt
+++ app/src/main/java/com/h/pixeldroid/MainActivity.kt
@@ -3,26 +3,13 @@ package com.h.pixeldroid
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.annotation.NonNull
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
-import com.google.android.material.navigation.NavigationView
-import com.h.pixeldroid.settings.ui.*
-import android.util.Log
-import com.h.pixeldroid.api.PixelfedAPI
-import com.h.pixeldroid.objects.Status
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import android.view.View
 import android.widget.Button
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -32,28 +19,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // setup the top toolbar
-        val toolbar : Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
         // Setup the drawer
         drawerLayout = findViewById(R.id.drawer_layout)
         val navigationView: NavigationView = findViewById(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener(this)
 
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar,
-            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        // On startup ONLY, start at the account settings
-        if(savedInstanceState == null) {
-            launchFragment(AccountFragment())
-            navigationView.setCheckedItem(R.id.nav_account)
-        }
-
-        val button_login = findViewById<Button>(R.id.button_start_login)
-        button_login.setOnClickListener((View.OnClickListener {
+        val buttonLogin = findViewById<Button>(R.id.button_start_login)
+        buttonLogin.setOnClickListener((View.OnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent) }))
 
@@ -68,9 +40,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
      */
     override fun onNavigationItemSelected(@NonNull item: MenuItem): Boolean {
         when (item.itemId){
-            R.id.nav_account -> launchFragment(AccountFragment())
-            R.id.nav_accessibility -> launchFragment(AccessibilityFragment())
-            R.id.nav_settings -> launchFragment(SettingsFragment())
+            R.id.nav_settings -> launchActivity(SettingsActivity())
+            R.id.nav_account -> launchActivity(ProfileActivity())
         }
 
         drawerLayout.closeDrawer(GravityCompat.START)
@@ -81,8 +52,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     /*
     Launches the given fragment and put it as the current "activity"
      */
-    private fun launchFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
+    private fun launchActivity(activity: AppCompatActivity) {
+        val intent = Intent(this, activity::class.java)
+        startActivity(intent)
     }
 
     /*
