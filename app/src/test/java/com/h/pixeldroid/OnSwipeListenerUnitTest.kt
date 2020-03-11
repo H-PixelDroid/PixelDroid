4acd3e3a24bb396e578ce912d8b7a145503c4bc4
Ulysse Widmer <ulysse.widmer@epfl.ch>
2020-03-11 10:52:18 +0900

    added unit tests for the Swipelistener class

---
+++ app/src/test/java/com/h/pixeldroid/OnSwipeListenerUnitTest.kt
@@ -0,0 +1,21 @@
+package com.h.pixeldroid
+
+import com.h.pixeldroid.motions.OnSwipeListener
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+class OnSwipeListenerUnitTest {
+
+    @Test
+    fun undefinedSwipeRightDoesNothingTest() {
+        val swipeListener = OnSwipeListener(null)
+        assertEquals(Unit, swipeListener.onSwipeRight())
+    }
+
+    @Test
+    fun undefinedSwipeLeftDoesNothingTest() {
+        val swipeListener = OnSwipeListener(null)
+        assertEquals(Unit, swipeListener.onSwipeLeft())
+    }
+}
