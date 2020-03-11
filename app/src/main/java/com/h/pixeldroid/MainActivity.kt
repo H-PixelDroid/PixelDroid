621e8072b279079fa9c89bda40378f64891624e4
Matthieu <61561059+Wv5twkFEKh54vo4tta9yu7dHa3@users.noreply.github.com>
2020-03-11 16:05:36 +0100

    Implement swipeable tabs

--- app/src/main/java/com/h/pixeldroid/MainActivity.kt
+++ app/src/main/java/com/h/pixeldroid/MainActivity.kt
@@ -11,60 +11,56 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.h.pixeldroid.fragments.HomeFragment
 import com.h.pixeldroid.fragments.ProfileFragment
-import com.h.pixeldroid.motions.OnSwipeListener
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val mainLinearLayout : LinearLayout = findViewById(R.id.main_linear_layout)
-        val homepageButton : ImageButton = findViewById(R.id.activity_main_home_btn)
-        val accountButton : ImageButton = findViewById(R.id.activity_main_account_btn)
-
-        homepageButton.setOnClickListener {
-            launchFragment(HomeFragment())
-        }
-        accountButton.setOnClickListener {
-            launchFragment(ProfileFragment())
-        }
 
         // Setup the drawer
         drawerLayout = findViewById(R.id.drawer_layout)
         val navigationView: NavigationView = findViewById(R.id.nav_view)
         navigationView.setNavigationItemSelectedListener(this)
 
-        val onSwipeListener = object: OnSwipeListener(this) {
-            override fun onSwipeRight() = swipeRight()
-            override fun onSwipeLeft() = swipeLeft()
+        viewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = object : FragmentStateAdapter(this) {
+            override fun createFragment(position: Int): Fragment {
+                return when(position){
+                    0 -> HomeFragment()
+                    4 -> ProfileFragment()
+                    else -> Fragment()
+                }
+            }
+
+            override fun getItemCount(): Int {
+                return 5
+            }
         }
-        mainLinearLayout.setOnTouchListener(onSwipeListener)
+        tabLayout = findViewById(R.id.tabs)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when(position){
+                0 -> tab.icon = getDrawable(R.drawable.ic_home_white_24dp)
+                1 -> tab.icon = getDrawable(R.drawable.ic_search_white_24dp)
+                2 -> tab.icon = getDrawable(R.drawable.ic_photo_camera_white_24dp)
+                3 -> tab.icon = getDrawable(R.drawable.ic_star_white_24dp)
+                4 -> tab.icon = getDrawable(R.drawable.ic_person_white_24dp)
 
-        // default fragment that displays when we open the app
-        launchFragment(HomeFragment())
-    }
-
-    private fun swipeRight() {
-        // TODO: correctly switch between tabs
-        drawerLayout.openDrawer(GravityCompat.START)
-    }
-
-    private fun swipeLeft() {
-        // TODO: correctly switch between tabs
-        launchFragment(ProfileFragment())
-    }
-
-    /*
-    Launches the given fragment and put it as the current "activity"
-     */
-    private fun launchFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
+            }
+        }.attach()
     }
 
     /*
@@ -73,7 +69,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(@NonNull item: MenuItem): Boolean {
         when (item.itemId){
             R.id.nav_settings -> launchActivity(SettingsActivity())
-            R.id.nav_account -> launchFragment(ProfileFragment())
         }
 
         drawerLayout.closeDrawer(GravityCompat.START)
