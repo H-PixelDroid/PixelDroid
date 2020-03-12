9c3ba711125f685119589306173ed9d409dde595
Matthieu <61561059+Wv5twkFEKh54vo4tta9yu7dHa3@users.noreply.github.com>
2020-03-12 23:22:25 +0100

    Add tests

--- app/src/androidTest/java/com/h/pixeldroid/ProfileTest.kt
+++ app/src/androidTest/java/com/h/pixeldroid/ProfileTest.kt
@@ -1,12 +1,17 @@
 package com.h.pixeldroid
 
+import android.content.Context
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import org.hamcrest.Matchers.not
+import org.junit.Before
 
 import org.junit.Rule
 import org.junit.Test
@@ -19,9 +24,22 @@ class ProfileTest {
     var activityRule: ActivityScenarioRule<MainActivity>
             = ActivityScenarioRule(MainActivity::class.java)
 
+
+    @Before
+    fun before(){
+        val preferences = InstrumentationRegistry.getInstrumentation()
+            .targetContext.getSharedPreferences("com.h.pixeldroid.pref", Context.MODE_PRIVATE)
+        preferences.edit().putString("accessToken", "azerty").apply()
+        preferences.edit().putString("domain", "localhost").apply()
+        ActivityScenario.launch(MainActivity::class.java)
+    }
     @Test
     fun testFollowersTextView() {
-        onView(withId(R.id.activity_main_account_btn)).perform(click())
+        onView(withId(R.id.view_pager)).perform(ViewActions.swipeLeft()).perform(ViewActions.swipeLeft()).perform(
+            ViewActions.swipeLeft()
+        ).perform(ViewActions.swipeLeft())
+
+        onView(withId(R.id.followButton)).perform(click())
         onView(withId(R.id.followers)).check(matches(withText("Followers")))
     }
 
