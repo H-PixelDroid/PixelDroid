471f8d42f7824b9c192958c85ffd826fa584e04d
Andrew Dobis <andrew.dobis@epfl.ch>
2020-03-12 22:49:53 +0100

    rebase on other branch

--- app/src/main/java/com/h/pixeldroid/fragments/PostFragment.kt
+++ app/src/main/java/com/h/pixeldroid/fragments/PostFragment.kt
@@ -1,10 +1,11 @@
-package com.h.pixeldroid
+package com.h.pixeldroid.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.h.pixeldroid.R
 import com.h.pixeldroid.models.Post
 import com.h.pixeldroid.models.Post.Companion.POST_TAG
 
