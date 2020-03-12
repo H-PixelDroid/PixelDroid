b56c9791cf61b2e68642f371f027ba9d9140e6fc
Andrea Clement <samuel.dietz@epfl.ch>
2020-03-12 22:49:50 +0100

    implemented texture listener

--- app/src/main/java/com/h/pixeldroid/CameraActivity.kt
+++ app/src/main/java/com/h/pixeldroid/CameraActivity.kt
@@ -3,35 +3,37 @@ package com.h.pixeldroid
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.hardware.camera2.*
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraDevice.TEMPLATE_PREVIEW
-import android.hardware.camera2.params.OutputConfiguration
-import android.hardware.camera2.params.SessionConfiguration
-import android.hardware.camera2.params.SessionConfiguration.SESSION_REGULAR
-import android.os.AsyncTask
-import android.os.Build
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
 import android.view.Surface
 import android.view.TextureView
+import android.view.TextureView.SurfaceTextureListener
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 
 
 class CameraActivity : AppCompatActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
-        val textureView = findViewById<TextureView>(R.id.textureView2)
+        val textureView = findViewById<TextureView>(R.id.textureView2)!!
         textureView.surfaceTextureListener
+
+    }
+
+
+    fun openCamera(surfaceTexture: SurfaceTexture) {
 
         val manager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
         val cameraCallback = object : CameraDevice.StateCallback() {
-            @RequiresApi(Build.VERSION_CODES.P)
             override fun onOpened(camera: CameraDevice) {
 
                 val sessionCallback = object : CameraCaptureSession.StateCallback() {
@@ -44,19 +46,17 @@ class CameraActivity : AppCompatActivity() {
 
                         val requestBuilder = camera.createCaptureRequest(TEMPLATE_PREVIEW)
 
-                        requestBuilder.addTarget(Surface(textureView.surfaceTexture))
+                        requestBuilder.addTarget(Surface(surfaceTexture))
                         session.setRepeatingRequest(requestBuilder.build(), null, null)
                     }
 
                 }
 
-                val outputConfigs =
-                    listOf<OutputConfiguration>(OutputConfiguration(Surface(textureView.surfaceTexture)))
-                camera.createCaptureSession(SessionConfiguration(
-                    SESSION_REGULAR,
-                    outputConfigs,
-                    AsyncTask.THREAD_POOL_EXECUTOR,
-                    sessionCallback))
+                camera.createCaptureSession(
+                    listOf(Surface(surfaceTexture)),
+                    sessionCallback,
+                    null
+                )
 
 
             }
@@ -76,7 +76,8 @@ class CameraActivity : AppCompatActivity() {
                 Manifest.permission.CAMERA
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            Toast.makeText(applicationContext,"Hello non-existant camera", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "Hello non-existant camera", Toast.LENGTH_SHORT)
+                .show()
 
             ActivityCompat.requestPermissions(
                 this,
@@ -91,4 +92,31 @@ class CameraActivity : AppCompatActivity() {
         }
         manager.openCamera(manager.cameraIdList[0], cameraCallback, null)
     }
+
+
+    var textureListener: SurfaceTextureListener = object : SurfaceTextureListener {
+        override fun onSurfaceTextureAvailable(
+            surface: SurfaceTexture,
+            width: Int,
+            height: Int
+        ) {
+            //open your camera here
+            openCamera(surface)
+        }
+
+        override fun onSurfaceTextureSizeChanged(
+            surface: SurfaceTexture,
+            width: Int,
+            height: Int
+        ) {
+            // Transform you image captured size according to the surface width and height
+        }
+
+        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+            return false
+        }
+
+        override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {}
+    }
+
 }
