fc7e4a864e96bc74a07642c551646f87d95d27db
Andrea Clement <samuel.dietz@epfl.ch>
2020-03-13 11:42:35 +0100

    camera not working, flow done, no feedback implemented

--- app/src/main/java/com/h/pixeldroid/CameraActivity.kt
+++ app/src/main/java/com/h/pixeldroid/CameraActivity.kt
@@ -8,7 +8,10 @@ import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraDevice.TEMPLATE_PREVIEW
 import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureRequest
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.view.Surface
 import android.view.TextureView
 import android.view.TextureView.SurfaceTextureListener
@@ -19,80 +22,19 @@ import androidx.core.app.ActivityCompat
 
 class CameraActivity : AppCompatActivity() {
 
+    private lateinit var textureView: TextureView
+    private lateinit var  cameraDevice: CameraDevice
+    private lateinit var requestBuilder: CaptureRequest.Builder
+    private lateinit var captureSession: CameraCaptureSession
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
-        val textureView = findViewById<TextureView>(R.id.textureView2)!!
+        textureView = findViewById<TextureView>(R.id.textureView2)!!
         textureView.surfaceTextureListener
 
     }
-
-
-    fun openCamera(surfaceTexture: SurfaceTexture) {
-
-        val manager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
-
-        val cameraCallback = object : CameraDevice.StateCallback() {
-            override fun onOpened(camera: CameraDevice) {
-
-                val sessionCallback = object : CameraCaptureSession.StateCallback() {
-                    override fun onConfigureFailed(session: CameraCaptureSession) {
-                        TODO("Not yet implemented")
-                    }
-
-                    override fun onConfigured(session: CameraCaptureSession) {
-
-
-                        val requestBuilder = camera.createCaptureRequest(TEMPLATE_PREVIEW)
-
-                        requestBuilder.addTarget(Surface(surfaceTexture))
-                        session.setRepeatingRequest(requestBuilder.build(), null, null)
-                    }
-
-                }
-
-                camera.createCaptureSession(
-                    listOf(Surface(surfaceTexture)),
-                    sessionCallback,
-                    null
-                )
-
-
-            }
-
-            override fun onDisconnected(camera: CameraDevice) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onError(camera: CameraDevice, error: Int) {
-                TODO("Not yet implemented")
-            }
-
-        }
-
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.CAMERA
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            Toast.makeText(applicationContext, "Hello non-existant camera", Toast.LENGTH_SHORT)
-                .show()
-
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(
-                    Manifest.permission.CAMERA,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-                ),
-                200
-            )
-
-            return
-        }
-        manager.openCamera(manager.cameraIdList[0], cameraCallback, null)
-    }
-
 
     var textureListener: SurfaceTextureListener = object : SurfaceTextureListener {
         override fun onSurfaceTextureAvailable(
@@ -119,4 +61,69 @@ class CameraActivity : AppCompatActivity() {
         override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {}
     }
 
+
+    fun openCamera(surfaceTexture: SurfaceTexture) {
+
+        val manager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+        val cameraCallback = object : CameraDevice.StateCallback() {
+            override fun onOpened(camera: CameraDevice) {
+                cameraDevice = camera
+
+                val sessionCallback = object : CameraCaptureSession.StateCallback() {
+                    override fun onConfigureFailed(session: CameraCaptureSession) {
+                        Toast.makeText(applicationContext,"Capture Session failed",Toast.LENGTH_SHORT).show();
+                    }
+
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        captureSession = session
+
+                        requestBuilder = camera.createCaptureRequest(TEMPLATE_PREVIEW)
+                        requestBuilder.addTarget(Surface(textureView.surfaceTexture))
+
+                        captureSession.setRepeatingRequest(requestBuilder.build(), null, null)
+                    }
+
+                }
+
+                cameraDevice.createCaptureSession(
+                    listOf(Surface(surfaceTexture)),
+                    sessionCallback,
+                    null
+                )
+
+
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {
+                Toast.makeText(applicationContext,"CameraDevice disconnected",Toast.LENGTH_SHORT).show();
+            }
+
+            override fun onError(camera: CameraDevice, error: Int) {
+                Toast.makeText(applicationContext,"CameraDevice error",Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Toast.makeText(applicationContext, "Hello non-existant camera", Toast.LENGTH_SHORT)
+                .show()
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
+                200
+            )
+
+            return
+        }
+        manager.openCamera(manager.cameraIdList[0], cameraCallback, null)
+    }
 }
