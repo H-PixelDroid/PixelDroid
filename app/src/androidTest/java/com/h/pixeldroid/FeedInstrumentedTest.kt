914b133aec2c9c53f92f4f3d167cd9217da98d63
Andrew Dobis <andrew.dobis@epfl.ch>
2020-03-13 10:36:07 +0100

    added a feed test

---
+++ app/src/androidTest/java/com/h/pixeldroid/FeedInstrumentedTest.kt
@@ -0,0 +1,38 @@
+package com.h.pixeldroid
+
+import android.content.Context
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.swipeDown
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class FeedInstrumentedTest {
+    @get:Rule
+    var activityRule: ActivityScenarioRule<MainActivity>
+            = ActivityScenarioRule(MainActivity::class.java)
+    @Before
+    fun before(){
+        val preferences = getInstrumentation()
+            .targetContext.getSharedPreferences("com.h.pixeldroid.pref", Context.MODE_PRIVATE)
+        preferences.edit().putString("accessToken", "azerty").apply()
+        preferences.edit().putString("domain", "localhost").apply()
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun swipingDownOnHomepageShowsMorePosts() {
+
+        val firstDesc = withId(R.id.description)
+        onView(withId(R.id.view_pager)).perform(swipeDown()).perform(swipeDown()).perform(swipeDown())
+        onView(withId(R.id.description)).check(matches(firstDesc))
+    }
+}
