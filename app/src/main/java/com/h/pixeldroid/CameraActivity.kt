0d5bb67e5454e3528428067a96bc972200925326
Andrea Clement <samuel.dietz@epfl.ch>
2020-03-14 08:58:05 +0100

    refactored code, still an activity

--- app/src/main/java/com/h/pixeldroid/CameraActivity.kt
+++ app/src/main/java/com/h/pixeldroid/CameraActivity.kt
@@ -4,14 +4,9 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.SurfaceTexture
-import android.hardware.camera2.CameraCaptureSession
-import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.*
 import android.hardware.camera2.CameraDevice.TEMPLATE_PREVIEW
-import android.hardware.camera2.CameraManager
-import android.hardware.camera2.CaptureRequest
 import android.os.Bundle
-import android.os.Handler
-import android.os.HandlerThread
 import android.view.Surface
 import android.view.TextureView
 import android.view.TextureView.SurfaceTextureListener
@@ -22,8 +17,10 @@ import androidx.core.app.ActivityCompat
 
 class CameraActivity : AppCompatActivity() {
 
-    private lateinit var textureView: TextureView
-    private lateinit var  cameraDevice: CameraDevice
+    private lateinit var    textureView: TextureView
+    private lateinit var surfaceTexture: SurfaceTexture
+
+    private lateinit var   cameraDevice: CameraDevice
     private lateinit var requestBuilder: CaptureRequest.Builder
     private lateinit var captureSession: CameraCaptureSession
 
@@ -31,29 +28,26 @@ class CameraActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
-        textureView = findViewById<TextureView>(R.id.textureView2)!!
-        textureView.surfaceTextureListener = textureListener;
+        textureView = findViewById(R.id.textureView2)!!
+        textureView.surfaceTextureListener = this.textureListener
 
     }
 
-    private var textureListener: SurfaceTextureListener = object : SurfaceTextureListener {
+    private var textureListener = object : SurfaceTextureListener {
         override fun onSurfaceTextureAvailable(
             surface: SurfaceTexture,
             width: Int,
             height: Int
         ) {
-            //open your camera here
-            Toast.makeText(applicationContext,"Capture Session failed",Toast.LENGTH_SHORT).show();
-            openCamera(surface)
+            surfaceTexture = surface
+            openCamera()
         }
 
         override fun onSurfaceTextureSizeChanged(
             surface: SurfaceTexture,
             width: Int,
             height: Int
-        ) {
-            // Transform you image captured size according to the surface width and height
-        }
+        ) {}
 
         override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
             return false
@@ -63,68 +57,74 @@ class CameraActivity : AppCompatActivity() {
     }
 
 
-    fun openCamera(surfaceTexture: SurfaceTexture) {
+    fun openCamera() {
 
         val manager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
-        val cameraCallback = object : CameraDevice.StateCallback() {
-            override fun onOpened(camera: CameraDevice) {
-                cameraDevice = camera
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
+                200
+            )
+            return
+        }
 
-                val sessionCallback = object : CameraCaptureSession.StateCallback() {
-                    override fun onConfigureFailed(session: CameraCaptureSession) {
-                        Toast.makeText(applicationContext,"Capture Session failed",Toast.LENGTH_SHORT).show();
-                    }
+        try {
+            manager.openCamera(manager.cameraIdList[0], cameraCallback, null)
+        } catch (e : CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
 
-                    override fun onConfigured(session: CameraCaptureSession) {
-                        captureSession = session
+    private val cameraCallback = object : CameraDevice.StateCallback() {
+        override fun onOpened(camera: CameraDevice) {
+            cameraDevice = camera
 
-                        requestBuilder = camera.createCaptureRequest(TEMPLATE_PREVIEW)
-                        requestBuilder.addTarget(Surface(textureView.surfaceTexture))
-
-                        captureSession.setRepeatingRequest(requestBuilder.build(), null, null)
-                    }
-
-                }
-
+            try {
                 cameraDevice.createCaptureSession(
                     listOf(Surface(surfaceTexture)),
                     sessionCallback,
                     null
                 )
-
-
+            } catch (e: CameraAccessException) {
+                e.printStackTrace()
             }
 
-            override fun onDisconnected(camera: CameraDevice) {
-                Toast.makeText(applicationContext,"CameraDevice disconnected",Toast.LENGTH_SHORT).show();
-            }
-
-            override fun onError(camera: CameraDevice, error: Int) {
-                Toast.makeText(applicationContext,"CameraDevice error",Toast.LENGTH_SHORT).show();
-            }
 
         }
 
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.CAMERA
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            Toast.makeText(applicationContext, "Hello non-existant camera", Toast.LENGTH_SHORT)
-                .show()
-
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(
-                    Manifest.permission.CAMERA,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-                ),
-                200
-            )
-
-            return
+        override fun onDisconnected(camera: CameraDevice) {
+            Toast.makeText(applicationContext,"CameraDevice disconnected",Toast.LENGTH_SHORT).show()
         }
-        manager.openCamera(manager.cameraIdList[0], cameraCallback, null)
+
+        override fun onError(camera: CameraDevice, error: Int) {
+            Toast.makeText(applicationContext,"CameraDevice error",Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+    private val sessionCallback = object : CameraCaptureSession.StateCallback() {
+        override fun onConfigureFailed(session: CameraCaptureSession) {
+            Toast.makeText(applicationContext,"Capture Session failed",Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onConfigured(session: CameraCaptureSession) {
+            captureSession = session
+
+            try {
+                requestBuilder = cameraDevice.createCaptureRequest(TEMPLATE_PREVIEW)
+                requestBuilder.addTarget(Surface(textureView.surfaceTexture))
+                captureSession.setRepeatingRequest(requestBuilder.build(), null, null)
+
+            } catch (e : CameraAccessException) {
+                e.printStackTrace()
+            }
+        }
+
+    }
+
 }
