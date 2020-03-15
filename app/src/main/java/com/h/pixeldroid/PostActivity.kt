d568925557f437bc85af33df2957dfb31fab146d
Andrew <andrew.dobis@epfl.ch>
2020-03-15 19:52:53 +0100

    fixed buggy postActivity

--- app/src/main/java/com/h/pixeldroid/PostActivity.kt
+++ app/src/main/java/com/h/pixeldroid/PostActivity.kt
@@ -2,6 +2,7 @@ package com.h.pixeldroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.SurfaceControl
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.h.pixeldroid.fragments.PostFragment
@@ -9,12 +10,16 @@ import com.h.pixeldroid.models.Post
 import com.h.pixeldroid.models.Post.Companion.POST_TAG
 
 class PostActivity : AppCompatActivity() {
+    lateinit var postFragment : PostFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
 
         val post = intent.getSerializableExtra(POST_TAG) as Post
-        post.setupPost(findViewById(R.id.postFragmentSingle), View(this))
+        postFragment = PostFragment.newInstance(post)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.postFragmentSingle, postFragment).commit()
     }
 }
