0ce4fffdf33445b23299eb4029fbb336bb2b36ce
Matthieu <61561059+Wv5twkFEKh54vo4tta9yu7dHa3@users.noreply.github.com>
2020-03-17 11:50:16 +0100

    Fix profile test

--- app/src/androidTest/java/com/h/pixeldroid/MyProfileTest.kt
+++ app/src/androidTest/java/com/h/pixeldroid/MyProfileTest.kt
@@ -10,17 +10,19 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import okhttp3.mockwebserver.RecordedRequest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 
-//@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4::class)
 class MyProfileTest {
-  /*  private val accountJson = "{\n" +
+    private val accountJson = "{\n" +
             "      \"id\": \"1450\",\n" +
             "      \"username\": \"deerbard_photo\",\n" +
             "      \"acct\": \"deerbard_photo\",\n" +
@@ -46,11 +48,20 @@ class MyProfileTest {
     @get:Rule
     var activityRule: ActivityScenarioRule<MainActivity>
             = ActivityScenarioRule(MainActivity::class.java)
+    private val dispatcher: Dispatcher = object : Dispatcher() {
+        @Throws(InterruptedException::class)
+        override fun dispatch(request: RecordedRequest): MockResponse {
+            when (request.path) {
+                "/api/v1/accounts/verify_credentials" -> return MockResponse().addHeader("Content-Type", "application/json; charset=utf-8").setResponseCode(200).setBody(accountJson)
+            }
+            return MockResponse().setResponseCode(404)
+        }
+    }
 
     @Before
     fun before(){
         val server = MockWebServer()
-        server.enqueue(MockResponse().addHeader("Content-Type", "application/json; charset=utf-8").setBody(accountJson))
+        server.dispatcher = dispatcher
         server.start()
         val baseUrl = server.url("")
         val preferences = InstrumentationRegistry.getInstrumentation()
@@ -65,8 +76,7 @@ class MyProfileTest {
             ViewActions.swipeLeft()
         ).perform(ViewActions.swipeLeft())
         Thread.sleep(1000)
-
         onView(withId(R.id.nbFollowersTextView)).check(matches(withText("68\nFollowers")))
         onView(withId(R.id.accountNameTextView)).check(matches(withText("deerbard_photo")))
-    }*/
+    }
 }
