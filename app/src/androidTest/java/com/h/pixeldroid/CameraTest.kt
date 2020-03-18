03a4cc59810ef71cda84c152f2b58477f97da64c
Andrea Clement <samuel.dietz@epfl.ch>
2020-03-18 12:00:48 +0100

    grant camera permission to app in camera test

--- app/src/androidTest/java/com/h/pixeldroid/CameraTest.kt
+++ app/src/androidTest/java/com/h/pixeldroid/CameraTest.kt
@@ -1,16 +1,27 @@
 package com.h.pixeldroid
 
-import androidx.fragment.app.testing.launchFragment
+import android.Manifest
+import android.os.Build
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
+import androidx.test.InstrumentationRegistry.getTargetContext
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import com.h.pixeldroid.fragments.CameraFragment
 import kotlinx.android.synthetic.main.fragment_camera.*
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import androidx.test.rule.GrantPermissionRule;
 
 @RunWith(AndroidJUnit4::class)
 class CameraTest {
+
+    @get:Rule
+    val mRuntimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(Manifest.permission.CAMERA)
+
+
     @Test
     fun testFragment() {
         val scenario = launchFragmentInContainer<CameraFragment>()
