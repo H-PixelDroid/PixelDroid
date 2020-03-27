0ac1ae3bfc106f113b2ce03092e35dc31db8f868
Andrea Clement <samuel.dietz@epfl.ch>
2020-03-27 11:10:26 +0100

    stoped swiping, now tests from fragment directly

--- app/src/androidTest/java/com/h/pixeldroid/CameraTest.kt
+++ app/src/androidTest/java/com/h/pixeldroid/CameraTest.kt
@@ -1,30 +1,28 @@
 package com.h.pixeldroid
 
 import android.Manifest
+import android.R
 import android.content.Context
-import android.os.Build
+import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.lifecycle.Lifecycle
-import androidx.test.InstrumentationRegistry.getTargetContext
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.rule.ActivityTestRule
+import androidx.test.rule.GrantPermissionRule
 import com.h.pixeldroid.fragments.CameraFragment
 import kotlinx.android.synthetic.main.fragment_camera.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import androidx.test.rule.GrantPermissionRule;
+
 
 @RunWith(AndroidJUnit4::class)
 class CameraTest {
@@ -38,29 +36,24 @@ class CameraTest {
             .targetContext.getSharedPreferences("com.h.pixeldroid.pref", Context.MODE_PRIVATE)
         preferences.edit().putString("accessToken", "azerty").apply()
         preferences.edit().putString("domain", "http://localhost").apply()
-        ActivityScenario.launch(MainActivity::class.java)
+
+
 
     }
 
     @Test
     fun  testTakePictureButton() {
-
-        Espresso.onView(ViewMatchers.withId(R.id.view_pager))
-            .perform(ViewActions.swipeLeft()).perform(ViewActions.swipeLeft())
-        Thread.sleep(1000)
-
-
-        Espresso.onView(withId(R.id.takePictureButton)).perform(click())
+        val scenario = launchFragmentInContainer<CameraFragment>()
+        scenario.onFragment { fragment ->
+        fragment.uploadPictureButton.performClick()
+        }
     }
 
     @Test
     fun testUploadPictureButton() {
-
-        Espresso.onView(ViewMatchers.withId(R.id.view_pager))
-            .perform(ViewActions.swipeLeft()).perform(ViewActions.swipeLeft())
-        Thread.sleep(1000)
-
-
-        Espresso.onView(withId(R.id.uploadPictureButton)).perform(click())
+        val scenario = launchFragmentInContainer<CameraFragment>()
+        scenario.onFragment { fragment ->
+            fragment.uploadPictureButton.performClick()
+        }
     }
 }
