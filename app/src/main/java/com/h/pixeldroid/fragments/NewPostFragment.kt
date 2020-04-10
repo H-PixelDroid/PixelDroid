a0c146bcc545cdc3792df4806e6b0c908bd18747
Ulysse Widmer <ulysse.widmer@epfl.ch>
2020-04-10 05:57:00 +0200

    refactor and test for PostCreationActivity

--- app/src/main/java/com/h/pixeldroid/fragments/NewPostFragment.kt
+++ app/src/main/java/com/h/pixeldroid/fragments/NewPostFragment.kt
@@ -4,26 +4,15 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.core.app.ActivityCompat
-import androidx.core.content.FileProvider
-import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import com.h.pixeldroid.PostCreationActivity
 import com.h.pixeldroid.R
-import java.io.File
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.*
 
 /**
  * This fragment is the entry point to create a post.
@@ -33,15 +22,9 @@ import java.util.*
  * add a description and more.
  */
 
-const val PICK_IMAGE_REQUEST = 1
-//const val IMAGE_CAPTURE_REQUEST = 2
-const val TAG = "New Post Fragment"
-
 class NewPostFragment : Fragment() {
 
-    private var pictureUri: Uri? = null
-//    lateinit var currentPhotoPath: String
-
+    private val PICK_IMAGE_REQUEST = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -54,87 +37,24 @@ class NewPostFragment : Fragment() {
             uploadPicture()
         }
 
-//        if (requireContext().packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)) {
-//            val takePictureButton: Button = view.findViewById(R.id.takePictureButton)
-//            takePictureButton.setOnClickListener{
-//                openCamera()
-//            }
-//        }
-
         return view
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == Activity.RESULT_OK
-            && data != null)
-            if (requestCode == PICK_IMAGE_REQUEST)
-                if (data.data != null)
-                    pictureUri = data.data
-                else Log.w(TAG, "Upload of picture failed.")
-//            else if (requestCode == IMAGE_CAPTURE_REQUEST) {
-//                try {
-//                    Log.e(TAG, "file: " + data.data.toString())
-//                    pictureUri = currentPhotoPath.toUri()
-//                }
-//                catch (err: IOException) {
-//                    Log.e(TAG, "Saving new picture failed: ${err.message}")
-//                }
-//            }
-//        else Log.e(TAG, "something went wrong")
-        if (pictureUri != null) {
+        if (resultCode == Activity.RESULT_OK && data != null
+            && requestCode == PICK_IMAGE_REQUEST  && data.data != null)
             startActivity(Intent(activity, PostCreationActivity::class.java)
-                    .putExtra("picture_uri", pictureUri)
+                .putExtra("picture_uri", data.data)
             )
-        }
     }
 
-//    @Throws(IOException::class)
-//    private fun createImageFile(): File {
-//        // Create an image file name
-//        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-//        val storageDir = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-//        return File.createTempFile(
-//            "JPEG_${timeStamp}_", /* prefix */
-//            ".jpg", /* suffix */
-//            storageDir /* directory */
-//        ).apply {
-//            // Save a file: path for use with ACTION_VIEW intents
-//            currentPhotoPath = absolutePath
-//        }
-//
-//    }
-
-
-//    @Throws(IOException::class)
-//    private fun createImageFile(): File {
-//        // Create an image file name
-//        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-//        val storageDir: File = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
-//        return File.createTempFile(
-//            "JPEG_${timeStamp}_", /* prefix */
-//            ".jpg", /* suffix */
-//            storageDir /* directory */
-//        ).apply {
-//            // Save a file: path for use with ACTION_VIEW intents
-//            currentPhotoPath = absolutePath
-//        }
-//    }
-
-
-
     private fun uploadPicture() {
-        val context = requireContext().applicationContext
         if (ActivityCompat.checkSelfPermission(
-                context,
-                Manifest.permission.READ_EXTERNAL_STORAGE)
-            != PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(
-                context,
+                requireContext(),
                 Manifest.permission.WRITE_EXTERNAL_STORAGE)
             != PackageManager.PERMISSION_GRANTED)
         {
             ActivityCompat.requestPermissions(requireActivity(), arrayOf(
-                Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
             ), 200)
             return
@@ -148,37 +68,4 @@ class NewPostFragment : Fragment() {
             )
         }
     }
-
-//    private fun openCamera() {
-////        val image = createImageFile()
-////        Intent().apply {
-////            action = MediaStore.ACTION_IMAGE_CAPTURE
-////            putExtra(MediaStore.EXTRA_OUTPUT, image.toUri())
-////            startActivityForResult(this, IMAGE_CAPTURE_REQUEST)
-////        }
-//        dispatchTakePictureIntent()
-//    }
-
-//    private fun dispatchTakePictureIntent() {
-//        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-//            // Ensure that there's a camera activity to handle the intent
-//            takePictureIntent.resolveActivity(requireContext().packageManager)?.also {
-//                // Create the File where the photo should go
-//                val photoFile: File? = try {
-//                    createImageFile()
-//                } catch (ex: IOException) {
-//                    // Error occurred while creating the File
-//                    null
-//                }
-//                // Continue only if the File was successfully created
-//                photoFile?.also {
-//                    val photoURI: Uri = currentPhotoPath.toUri()
-//                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-//                    startActivityForResult(takePictureIntent, IMAGE_CAPTURE_REQUEST)
-//                }
-//            }
-//        }
-//    }
-
-
 }
