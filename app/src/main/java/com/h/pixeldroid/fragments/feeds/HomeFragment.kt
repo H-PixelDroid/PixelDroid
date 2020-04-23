965830c79d855c992a2c4b8a514a7e09866d59cc
Andrew <andrew.dobis@epfl.ch>
2020-04-23 17:50:10 +0200

    merged with master again

--- app/src/main/java/com/h/pixeldroid/fragments/feeds/HomeFragment.kt
+++ app/src/main/java/com/h/pixeldroid/fragments/feeds/HomeFragment.kt
@@ -8,7 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.paging.LivePagedListBuilder
