83ce37878f973c6569c5cc4e9a39669711a28868
Andrea Clement <samuel.dietz@epfl.ch>
2020-04-30 14:40:38 +0200

    request camera permissions

--- app/src/main/java/com/h/pixeldroid/CameraActivity.kt
+++ app/src/main/java/com/h/pixeldroid/CameraActivity.kt
@@ -1,4 +1,88 @@
 package com.h.pixeldroid
+// Your IDE likely can auto-import these classes, but there are several
+// different implementations so we list them here to disambiguate.
 
-class CameraActivity {
+import android.Manifest
+import android.content.pm.PackageManager
+import android.util.Size
+import android.graphics.Matrix
+import android.os.Bundle
+import android.view.TextureView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+// This is an arbitrary number we are using to keep track of the permission
+// request. Where an app has multiple context for requesting permission,
+// this can help differentiate the different contexts.
+private const val REQUEST_CODE_PERMISSIONS = 10
+
+// This is an array of all the permission specified in the manifest.
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+class CameraActivity : AppCompatActivity(), LifecycleOwner {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Add this at the end of onCreate function
+
+    viewFinder = findViewById(R.id.view_finder)
+
+    // Request camera permissions
+    if (allPermissionsGranted()) {
+        viewFinder.post { startCamera() }
+    } else {
+        ActivityCompat.requestPermissions(
+            this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+    }
+
+    // Every time the provided texture view changes, recompute layout
+    viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+        updateTransform()
+    }
+   }
+
+    // Add this after onCreate
+
+    private val executor = Executors.newSingleThreadExecutor()
+    private lateinit var viewFinder: TextureView
+
+    private fun startCamera() {
+        // TODO: Implement CameraX operations
+    }
+
+    private fun updateTransform() {
+        // TODO: Implement camera viewfinder transformations
+    }
+
+    /**
+     * Process result from permission request dialog box, has the request
+     * been granted? If yes, start Camera. Otherwise display a toast
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                viewFinder.post { startCamera() }
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    /**
+     * Check if all permission specified in the manifest have been granted
+     */
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+               baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
 }
