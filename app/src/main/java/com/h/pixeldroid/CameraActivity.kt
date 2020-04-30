53dbe454620d10fd042fc80e8f9c4fcebb4c57ac
Andrea Clement <samuel.dietz@epfl.ch>
2020-04-30 14:48:41 +0200

    implemented basic viewFinder aka stream preview

--- app/src/main/java/com/h/pixeldroid/CameraActivity.kt
+++ app/src/main/java/com/h/pixeldroid/CameraActivity.kt
@@ -7,9 +7,14 @@ import android.content.pm.PackageManager
 import android.util.Size
 import android.graphics.Matrix
 import android.os.Bundle
+import android.view.Surface
 import android.view.TextureView
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
@@ -53,11 +58,53 @@ class CameraActivity : AppCompatActivity(), LifecycleOwner {
     private lateinit var viewFinder: TextureView
 
     private fun startCamera() {
-        // TODO: Implement CameraX operations
+        // Create configuration object for the viewfinder use case
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetResolution(Size(640, 480))
+        }.build()
+
+
+        // Build the viewfinder use case
+        val preview = Preview(previewConfig)
+
+        // Every time the viewfinder is updated, recompute layout
+        preview.setOnPreviewOutputUpdateListener {
+
+            // To update the SurfaceTexture, we have to remove it and re-add it
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+        }
+
+        // Bind use cases to lifecycle
+        // If Android Studio complains about "this" being not a LifecycleOwner
+        // try rebuilding the project or updating the appcompat dependency to
+        // version 1.1.0 or higher.
+        CameraX.bindToLifecycle(this, preview)
     }
 
     private fun updateTransform() {
-        // TODO: Implement camera viewfinder transformations
+        val matrix = Matrix()
+
+        // Compute the center of the view finder
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        // Correct preview output to account for display rotation
+        val rotationDegrees = when(viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        // Finally, apply transformations to our TextureView
+        viewFinder.setTransform(matrix)
     }
 
     /**
