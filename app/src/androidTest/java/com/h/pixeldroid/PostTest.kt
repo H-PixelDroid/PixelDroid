1cba0843aab4de75fe52b64bd1386390123fd575
Ulysse Widmer <ulysse.widmer@epfl.ch>
2020-04-30 19:43:29 +0200

    added test for album

--- app/src/androidTest/java/com/h/pixeldroid/PostTest.kt
+++ app/src/androidTest/java/com/h/pixeldroid/PostTest.kt
@@ -27,12 +27,14 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class PostTest {
 
+    private lateinit var context: Context
+
     @get:Rule
     var globalTimeout: Timeout = Timeout.seconds(100)
 
     @Before
     fun before(){
-        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        context = InstrumentationRegistry.getInstrumentation().targetContext
         val mockServer = MockServer()
         mockServer.start()
         val baseUrl = mockServer.getUrl()
@@ -41,6 +43,10 @@ class PostTest {
             Context.MODE_PRIVATE)
         preferences.edit().putString("accessToken", "azerty").apply()
         preferences.edit().putString("domain", baseUrl.toString()).apply()
+    }
+
+    @Test
+    fun saveToGalleryTestSimplePost() {
         val attachment = Attachment(
             id = "12",
             url = "https://wiki.gnugen.ch/lib/tpl/gnugen/images/logo_web.png"
@@ -56,15 +62,42 @@ class PostTest {
         val intent = Intent(context, PostActivity::class.java)
         intent.putExtra(Status.POST_TAG, post)
         ActivityScenario.launch<PostActivity>(intent)
-    }
-
-    @Test
-    fun saveToGalleryTest() {
         onView(withId(R.id.postPicture)).perform(longClick())
         onView(withText(R.string.save_to_gallery)).inRoot(RootMatchers.isPlatformPopup()).perform(click())
         Thread.sleep(300)
         onView(withText(R.string.image_download_downloading)).inRoot(
             RootMatchers.hasWindowLayoutParams()
         ).check(matches(isDisplayed()))
+        Thread.sleep(5000)
+    }
+
+    @Test
+    fun saveToGalleryTestAlbum() {
+        val attachment1 = Attachment(
+            id = "12",
+            url = "https://wiki.gnugen.ch/lib/tpl/gnugen/images/logo_web.png"
+        )
+        val attachment2 = Attachment(
+            id = "13",
+            url = "https://wiki.gnugen.ch/lib/tpl/gnugen/images/logo_web.png"
+        )
+        val post = Status(
+            id = "12",
+            account = Account(
+                id = "12",
+                username = "douze"
+            ),
+            media_attachments = listOf(attachment1, attachment2)
+        )
+        val intent = Intent(context, PostActivity::class.java)
+        intent.putExtra(Status.POST_TAG, post)
+        ActivityScenario.launch<PostActivity>(intent)
+        onView(withId(R.id.imageImageView)).perform(longClick())
+        onView(withText(R.string.save_to_gallery)).inRoot(RootMatchers.isPlatformPopup()).perform(click())
+        Thread.sleep(300)
+        onView(withText(R.string.image_download_downloading)).inRoot(
+            RootMatchers.hasWindowLayoutParams()
+        ).check(matches(isDisplayed()))
+        Thread.sleep(5000)
     }
 }
