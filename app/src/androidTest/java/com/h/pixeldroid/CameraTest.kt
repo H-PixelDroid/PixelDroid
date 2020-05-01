bcb806cc82a54d5c31b8e6cdcb5a67d6ada1cd57
Andrea Clement <samuel.dietz@epfl.ch>
2020-05-01 11:00:29 +0200

    added test with device orientation

--- app/src/androidTest/java/com/h/pixeldroid/CameraTest.kt
+++ app/src/androidTest/java/com/h/pixeldroid/CameraTest.kt
@@ -10,6 +10,8 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.Timeout
 import androidx.lifecycle.Lifecycle
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.uiautomator.UiDevice
 import com.h.pixeldroid.CameraActivity
 import com.h.pixeldroid.PostCreationActivity
 import com.h.pixeldroid.R
@@ -19,14 +21,22 @@ class CameraTest {
     @get:Rule
     var globalTimeout: Timeout = Timeout.seconds(30)
     @get:Rule
-    var activityRule = ActivityScenarioRule<CameraActivity>(CameraActivity::class.java)
+    var activityRule = ActivityScenarioRule(CameraActivity::class.java)
 
 
     @Test
     fun takePictureButton() {
-        // Start the screen of your activity.
         onView(withId(R.id.capture_button)).perform(click())
         Thread.sleep(300)
-        assert(activityRule.scenario.state.equals(Lifecycle.State.DESTROYED))
+        assert(activityRule.scenario.state == Lifecycle.State.DESTROYED)
+    }
+
+    @Test
+    fun rotateAndTakePictureButton() {
+        val device = UiDevice.getInstance(getInstrumentation())
+        device.setOrientationLeft()
+        onView(withId(R.id.capture_button)).perform(click())
+        Thread.sleep(300)
+        assert(activityRule.scenario.state == Lifecycle.State.DESTROYED)
     }
 }
