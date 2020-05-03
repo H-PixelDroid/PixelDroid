884c56bd5726cf667fdbacf71a66524e1addea46
Matthieu De Beule <61561059+Wv5twkFEKh54vo4tta9yu7dHa3@users.noreply.github.com>
2020-05-03 21:11:43 +0200

    Fix camera test

--- app/src/androidTest/java/com/h/pixeldroid/CameraTest.kt
+++ app/src/androidTest/java/com/h/pixeldroid/CameraTest.kt
@@ -6,19 +6,20 @@ import org.junit.Rule
 import org.junit.Test
 import androidx.test.rule.GrantPermissionRule
 import com.h.pixeldroid.fragments.CameraFragment
+import kotlinx.android.synthetic.main.camera_ui_container.*
 import kotlinx.android.synthetic.main.fragment_camera.*
 
 class CameraTest {
 
     @get:Rule
-    val mRuntimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(Manifest.permission.CAMERA)
+    val mRuntimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE)
 
     @Test
     fun takePictureButton() {
         val scenario = launchFragmentInContainer<CameraFragment>()
         scenario.onFragment { fragment ->
-            fragment.capture_button.performClick()
-            Thread.sleep(300)
+            fragment.camera_capture_button.performClick()
+            Thread.sleep(3000)
             assert(fragment.isHidden)
         }
     }
@@ -27,7 +28,7 @@ class CameraTest {
     fun uploadButton() {
         val scenario = launchFragmentInContainer<CameraFragment>()
         scenario.onFragment { fragment ->
-            fragment.upload_button.performClick()
+            fragment.photo_view_button.performClick()
             Thread.sleep(300)
             assert(fragment.isHidden)
         }
