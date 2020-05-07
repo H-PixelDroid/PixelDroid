6c8b3c61d2151ff7e5ba8e22d5b9bfe9f5983538
Andrew <andrew.dobis@epfl.ch>
2020-05-07 19:20:59 +0200

    added night mode selector in settings, but doesn't seem to work

--- app/src/main/java/com/h/pixeldroid/SettingsActivity.kt
+++ app/src/main/java/com/h/pixeldroid/SettingsActivity.kt
@@ -2,11 +2,17 @@ package com.h.pixeldroid
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat.recreate
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.settings)
         supportFragmentManager
             .beginTransaction()
@@ -18,6 +24,34 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            val preference : ListPreference? = preferenceManager.findPreference("Theme")
+            preference?.setOnPreferenceChangeListener { _, newValue ->
+                val themes = resources.getStringArray(R.array.theme_values)
+                when(newValue.toString()) {
+                    //Default
+                    themes[0] -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                        true
+                    }
+                    //Light
+                    themes[1] -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                        true
+                    }
+                    //Dark
+                    themes[2] -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                        true
+                    }
+                    else -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                        false
+                    }
+
+                }
+
+            }
         }
     }
 
