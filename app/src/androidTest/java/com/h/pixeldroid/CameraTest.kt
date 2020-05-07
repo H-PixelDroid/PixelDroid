b7cc0734f485082edcca5e7fd3c6abe018a23b3e
Andrea Clement <samuel.dietz@epfl.ch>
2020-05-07 17:22:18 +0200

    brought back fragment scenario

--- app/src/androidTest/java/com/h/pixeldroid/CameraTest.kt
+++ app/src/androidTest/java/com/h/pixeldroid/CameraTest.kt
@@ -1,80 +1,71 @@
 package com.h.pixeldroid
 
 import android.Manifest
-import android.content.Intent
-import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.swipeRight
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.Intents.intending
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
-import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import android.content.Context
+import androidx.core.view.isVisible
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.GrantPermissionRule
-import com.google.android.material.tabs.TabLayout
-import org.hamcrest.Matcher
+import com.h.pixeldroid.fragments.CameraFragment
+import kotlinx.android.synthetic.main.fragment_camera.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.Timeout
+import org.junit.runner.RunWith
 
+
+@RunWith(AndroidJUnit4::class)
 class CameraTest {
-
-    private lateinit var activityScenario: ActivityScenario<MainActivity>
 
     @get:Rule
     val mRuntimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(Manifest.permission.CAMERA)
-    @get:Rule
-    var globalTimeout: Timeout = Timeout.seconds(30)
-    @get:Rule
-    var runtimePermissionRule: GrantPermissionRule =
-        GrantPermissionRule.grant(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-    @get:Rule
-    var intentsTestRule: IntentsTestRule<MainActivity> =
-        IntentsTestRule(MainActivity::class.java)
-    @get:Rule
-    var activityRule: ActivityScenarioRule<MainActivity>
-            = ActivityScenarioRule(MainActivity::class.java)
 
     @Before
-    fun setup() {
-        activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        activityScenario.onActivity {
-                a -> a.findViewById<TabLayout>(R.id.tabs).getTabAt(2)?.select()
+    fun before(){
+        val preferences = InstrumentationRegistry.getInstrumentation()
+            .targetContext.getSharedPreferences("com.h.pixeldroid.pref", Context.MODE_PRIVATE)
+        preferences.edit().putString("accessToken", "azerty").apply()
+        preferences.edit().putString("domain", "http://localhost").apply()
+    }
+
+    @Test
+    fun  takePictureButton() {
+        val scenario = launchFragmentInContainer<CameraFragment>()
+        scenario.onFragment { fragment ->
+        fragment.capture_button.performClick()
         }
-        Thread.sleep(1000)
     }
 
     @Test
-    fun flipCameraButton() {
-        onView(withId(R.id.flip_button)).check(matches(isClickable()))
-        onView(withId(R.id.flip_button)).perform(click())
+    fun uploadPictureButton() {
+        val scenario = launchFragmentInContainer<CameraFragment>()
+        scenario.moveToState(Lifecycle.State.CREATED)
+        scenario.onFragment { fragment ->
+            fragment.upload_button.performClick()
+            assert(!fragment.isVisible)
+            assert(fragment.requireActivity().window.decorView.rootView.isVisible)
+        }
     }
 
     @Test
-    fun seekBar() {
-        onView(withId(R.id.seekBar)).check(matches(isFocusable()))
-        onView(withId(R.id.seekBar)).perform(swipeRight())
+    fun openCameraAndTakePicture() {
+        val scenario = launchFragmentInContainer<CameraFragment>()
+        scenario.moveToState(Lifecycle.State.CREATED)
+        scenario.onFragment { fragment ->
+            val activity = fragment.requireActivity()
+            fragment.onCreateView(activity.layoutInflater, activity.findViewById(android.R.id.content), null)
+        }
     }
-
-  //  @Test
-  //  fun takePictureButton() {
-  //      onView(withId(R.id.capture_button)).perform(click())
-  //      val expectedIntent: Matcher<Intent> = hasComponent(PostCreationActivity::class.simpleName)
-  //      intending(expectedIntent)
-  //      Thread.sleep(1000)
-  //      intended(expectedIntent)
-  //  }
 
     @Test
-    fun uploadButtonLaunchesGalleryIntent() {
-        val expectedIntent: Matcher<Intent> = hasAction(Intent.ACTION_CHOOSER)
-        intending(expectedIntent)
-        onView(withId(R.id.upload_button)).perform(click())
-        Thread.sleep(1000)
-        intended(expectedIntent)
+    fun switchCameraButton() {
+        val scenario = launchFragmentInContainer<CameraFragment>()
+        scenario.moveToState(Lifecycle.State.CREATED)
+        scenario.onFragment { fragment ->
+            fragment.flip_button.performClick()
+        }
     }
+
 }
