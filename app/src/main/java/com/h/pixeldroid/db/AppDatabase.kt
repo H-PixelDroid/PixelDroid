b024255522b2a90e7937bc9c07c362b321952cc7
Ulysse Widmer <ulysse.widmer@epfl.ch>
2020-05-08 11:09:18 +0200

    rework post entity

--- app/src/main/java/com/h/pixeldroid/db/AppDatabase.kt
+++ app/src/main/java/com/h/pixeldroid/db/AppDatabase.kt
@@ -1,39 +1,15 @@
 package com.h.pixeldroid.db
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @Database(entities = [PostEntity::class], version = 1)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun postDao(): PostDao
-    val MAX_NUMBER_OF_POSTS = 100
-
     companion object {
-        // Singleton prevents multiple instances of database opening at the
-        // same time.
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
-        var TEST_MODE = false
-
-        fun getDatabase(context: Context): AppDatabase {
-
-            return INSTANCE ?: synchronized(this) {
-                // To be able to create a temporary database that flushes when tests are over
-                var instance = if (TEST_MODE) {
-                    Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).allowMainThreadQueries().build()
-                } else {
-                    Room.databaseBuilder(
-                        context.applicationContext, AppDatabase::class.java, "posts_database"
-                    ).build()
-                }
-
-                INSTANCE = instance
-                return instance
-            }
-        }
+        const val MAX_NUMBER_OF_POSTS = 100
     }
+
+    abstract fun postDao(): PostDao
 }
--- app/src/main/java/com/h/pixeldroid/db/PostEntity.kt
+++ app/src/main/java/com/h/pixeldroid/db/PostEntity.kt
@@ -1,17 +1,39 @@
 package com.h.pixeldroid.db
 
-import androidx.room.ColumnInfo;
-import androidx.room.Entity;
-import androidx.room.PrimaryKey;
-import java.util.Date
+import androidx.room.Embedded
+import androidx.room.Entity
 
-@Entity(tableName= "posts")
+data class Account (
+    val account_id: String,
+    val username: String,
+    val acct: String = "",
+    val account_url: String = "", //HTTPS URL
+    val display_name: String? = null,
+    val note: String = "", //HTML
+    val avatar: String = "", //URL
+    val avatar_static: String = "" //URL
+)
+
+@Entity(tableName= "posts", primaryKeys = ["domain", "id"])
 data class PostEntity(
-    @PrimaryKey val uid: Int,
-    @ColumnInfo(name = "domain") val domain: String? = "",
-    @ColumnInfo(name = "username") val username: String? = "",
-    @ColumnInfo(name = "display name") val displayName: String? = "",
-    @ColumnInfo(name = "accountID") val accountID: Int? = -1,
-    @ColumnInfo(name = "image url") val ImageURL: String? = "",
-    @ColumnInfo(name = "date") val date: Date?
+    val domain: String,
+    val id: String,
+    val uri: String = "",
+    val created_at: String = "", //ISO 8601 Datetime (maybe can use a date type)
+    @Embedded val account: Account,
+    val content: String = "", //HTML
+    val spoiler_text: String = "",
+    val reblogs_count: Int = 0,
+    val favourites_count: Int = 0,
+    val replies_count: Int = 0,
+    val url: String? = null, //URL
+    val in_reply_to_id: String? = null,
+    val in_reply_to_account: String? = null,
+    val text: String? = null,
+    //Authorized user attributes
+    val favourited: Boolean = false,
+    val reblogged: Boolean = false,
+    val muted: Boolean = false,
+    val bookmarked: Boolean = false,
+    val pinned: Boolean = false
 )
