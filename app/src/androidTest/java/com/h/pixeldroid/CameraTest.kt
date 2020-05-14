efae3c452120cfccc27c65a4589eff787288ad61
Matthieu De Beule <61561059+Wv5twkFEKh54vo4tta9yu7dHa3@users.noreply.github.com>
2020-05-14 09:24:38 +0200

    better tests?

--- app/src/androidTest/java/com/h/pixeldroid/CameraTest.kt
+++ app/src/androidTest/java/com/h/pixeldroid/CameraTest.kt
@@ -1,37 +1,66 @@
 package com.h.pixeldroid
 
 import android.Manifest
+import android.content.Intent
+import android.content.Intent.ACTION_CHOOSER
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
 import org.junit.Rule
 import org.junit.Test
 import androidx.test.rule.GrantPermissionRule
 import com.h.pixeldroid.fragments.CameraFragment
 import kotlinx.android.synthetic.main.camera_ui_container.*
 import kotlinx.android.synthetic.main.fragment_camera.*
+import org.hamcrest.CoreMatchers
+import org.hamcrest.Matcher
+import org.junit.After
+import org.junit.Before
 
 class CameraTest {
 
     @get:Rule
     val mRuntimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE)
 
+    @Before
+    fun before(){
+        Intents.init()
+    }
+    @After
+    fun after(){
+        Intents.release()
+    }
     @Test
     fun takePictureButton() {
+        val expectedIntent: Matcher<Intent> = CoreMatchers.allOf(
+            IntentMatchers.hasComponent("com.h.pixeldroid.PhotoEditActivity")
+        )
         val scenario = launchFragmentInContainer<CameraFragment>()
+        Thread.sleep(1000)
         scenario.onFragment { fragment ->
             fragment.camera_capture_button.performClick()
-            Thread.sleep(3000)
-            assert(fragment.isHidden)
         }
+        Thread.sleep(2000)
+        Intents.intended(expectedIntent)
+
     }
 
     @Test
     fun uploadButton() {
+        val expectedIntent: Matcher<Intent> = CoreMatchers.allOf(
+            IntentMatchers.hasAction(ACTION_CHOOSER)
+        )
+
         val scenario = launchFragmentInContainer<CameraFragment>()
         scenario.onFragment { fragment ->
             fragment.photo_view_button.performClick()
-            Thread.sleep(300)
-            assert(fragment.isHidden)
         }
+        Thread.sleep(1000)
+
+        Intents.intended(expectedIntent)
+
+
+
     }
 
     @Test
@@ -39,7 +68,9 @@ class CameraTest {
         val scenario = launchFragmentInContainer<CameraFragment>()
         scenario.onFragment { fragment ->
             fragment.camera_switch_button.performClick()
-            Thread.sleep(300)
+        }
+        Thread.sleep(1000)
+        scenario.onFragment { fragment ->
             assert(!fragment.isHidden)
         }
     }
