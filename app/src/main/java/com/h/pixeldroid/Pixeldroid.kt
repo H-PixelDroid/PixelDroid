cd3782d55fe682c4c4f5b62271f59711802630b1
Andrew <andrew.dobis@epfl.ch>
2020-05-14 19:16:23 +0200

    added night mode for splashscreen

--- app/src/main/java/com/h/pixeldroid/Pixeldroid.kt
+++ app/src/main/java/com/h/pixeldroid/Pixeldroid.kt
@@ -1,11 +1,12 @@
 package com.h.pixeldroid
 
+import android.Manifest
 import android.app.Application
+import android.view.WindowManager
 import androidx.preference.PreferenceManager
 import com.h.pixeldroid.utils.ThemeUtils
 
 class Pixeldroid: Application() {
-
     override fun onCreate() {
         super.onCreate()
         val sharedPreferences =
