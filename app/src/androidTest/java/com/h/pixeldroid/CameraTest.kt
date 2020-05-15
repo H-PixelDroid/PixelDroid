2621788241c67bb7d27945a27e1d060fdf9edb02
Matthieu De Beule <61561059+Wv5twkFEKh54vo4tta9yu7dHa3@users.noreply.github.com>
2020-05-15 11:03:53 +0200

    save image so that ci can go through gallery

--- app/src/androidTest/java/com/h/pixeldroid/CameraTest.kt
+++ app/src/androidTest/java/com/h/pixeldroid/CameraTest.kt
@@ -1,26 +1,43 @@
 package com.h.pixeldroid
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
 import android.content.Intent.ACTION_CHOOSER
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.media.MediaScannerConnection
+import android.os.Environment
+import android.provider.MediaStore
+import android.webkit.MimeTypeMap
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
-import org.junit.Rule
-import org.junit.Test
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.GrantPermissionRule
 import com.h.pixeldroid.fragments.CameraFragment
 import kotlinx.android.synthetic.main.camera_ui_container.*
-import kotlinx.android.synthetic.main.fragment_camera.*
 import org.hamcrest.CoreMatchers
 import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.io.File
+import java.io.OutputStream
+
 
 class CameraTest {
 
     @get:Rule
-    val mRuntimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE)
+    val mRuntimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+    private fun File.writeBitmap(bitmap: Bitmap) {
+        outputStream().use { out ->
+            bitmap.compress(Bitmap.CompressFormat.PNG, 85, out)
+            out.flush()
+        }
+    }
 
     @Before
     fun before(){
@@ -32,7 +49,31 @@ class CameraTest {
     }
     @Test
     fun takePictureButton() {
-        val scenario = launchFragmentInContainer<CameraFragment>()
+        var scenario = launchFragmentInContainer<CameraFragment>()
+
+        scenario.onFragment { _ ->
+            val image = Bitmap.createBitmap(500, 500, Bitmap.Config.ARGB_8888)
+            image.eraseColor(Color.GREEN)
+            val folder =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
+            if (!folder.exists()) {
+                folder.mkdir()
+            }
+            val file = File.createTempFile("temp_img", ".png", folder)
+            file.writeBitmap(image)
+
+            val context = InstrumentationRegistry.getInstrumentation().targetContext
+            val mimeType = MimeTypeMap.getSingleton()
+                .getMimeTypeFromExtension(file.extension)
+            MediaScannerConnection.scanFile(
+                context,
+                arrayOf(file.absolutePath),
+                arrayOf(mimeType)){_, _ ->
+            }
+
+        }
+        scenario = launchFragmentInContainer<CameraFragment>()
+
         Thread.sleep(2000)
         scenario.onFragment { fragment ->
             fragment.camera_capture_button.performClick()
