40baf6abeb57614095f466038e68e226fb12c31b
Matthieu <61561059+Wv5twkFEKh54vo4tta9yu7dHa3@users.noreply.github.com>
2020-06-05 19:10:20 +0200

    Sort when getting statuses

--- app/src/main/java/com/h/pixeldroid/db/PostDao.kt
+++ app/src/main/java/com/h/pixeldroid/db/PostDao.kt
@@ -13,7 +13,7 @@ interface PostDao {
     @Query("SELECT COUNT(*) FROM posts WHERE user_id=:userId AND instance_uri=:instanceUri")
     fun numberOfPosts(userId: String, instanceUri: String): Int
 
-    @Query("SELECT * FROM posts WHERE user_id=:user AND instance_uri=:instanceUri")
+    @Query("SELECT * FROM posts WHERE user_id=:user AND instance_uri=:instanceUri ORDER BY date DESC")
     fun getAll(user: String, instanceUri: String): List<PostDatabaseEntity>
 
     @Query("SELECT COUNT(*) FROM posts WHERE uri=:uri AND user_id=:userId AND instance_uri=:instanceUri")
