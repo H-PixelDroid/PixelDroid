0910ef176a8b7bf8d20d60fbcd665cf3764b67d7
Matthieu <24-artectrex@users.noreply.shinice.net>
2020-09-25 16:46:01 +0200

    Create About activity with licenses and dependency list

--- app/src/main/java/com/h/pixeldroid/SettingsActivity.kt
+++ app/src/main/java/com/h/pixeldroid/SettingsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import com.h.pixeldroid.utils.ThemeUtils.Companion.setThemeFromPreferences
@@ -29,12 +30,16 @@ class SettingsActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferen
 
     override fun onResume() {
         super.onResume()
-        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this)
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(
+            this
+        )
     }
 
     override fun onPause() {
         super.onPause()
-        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this)
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(
+            this
+        )
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
@@ -49,6 +54,13 @@ class SettingsActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferen
      class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            val button: Preference? = findPreference("about")
+            button?.onPreferenceClickListener =
+                Preference.OnPreferenceClickListener {
+                    val intent = Intent(context, AboutActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
         }
     }
 
