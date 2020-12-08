e71e5c304a655939de020fe0e8e33d1b418ae80f
Matthieu <24-artectrex@users.noreply.shinice.net>
2020-12-08 18:33:16 +0100

    clean imports

--- app/src/main/java/com/h/pixeldroid/LoginActivity.kt
+++ app/src/main/java/com/h/pixeldroid/LoginActivity.kt
@@ -30,10 +30,8 @@ import okhttp3.HttpUrl
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Inject
+
 /**
 Overview of the flow of the login process: (boxes are requests done in parallel,
 since they do not depend on each other)
