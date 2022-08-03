62dc91413de09e2737f1cc2990a23f39c3155f4d
Matthieu <24-artectrex@users.noreply.shinice.net>
2022-08-03 20:19:29 +0200

    improve on comments

--- app/src/main/java/org/pixeldroid/app/posts/PostActivity.kt
+++ app/src/main/java/org/pixeldroid/app/posts/PostActivity.kt
@@ -1,28 +1,16 @@
 package org.pixeldroid.app.posts
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.view.WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE
 import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import androidx.paging.ExperimentalPagingApi
-import androidx.paging.PagingDataAdapter
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.Job
 import org.pixeldroid.app.R
 import org.pixeldroid.app.databinding.ActivityPostBinding
-import org.pixeldroid.app.databinding.CommentBinding
-import org.pixeldroid.app.posts.feeds.initAdapter
-import org.pixeldroid.app.posts.feeds.launch
-import org.pixeldroid.app.posts.feeds.uncachedFeeds.FeedViewModel
-import org.pixeldroid.app.posts.feeds.uncachedFeeds.comments.CommentContentRepository
-import org.pixeldroid.app.profile.ProfileViewModelFactory
+import org.pixeldroid.app.posts.feeds.uncachedFeeds.comments.CommentFragment
+import org.pixeldroid.app.posts.feeds.uncachedFeeds.comments.CommentFragment.Companion.COMMENT_DOMAIN
+import org.pixeldroid.app.posts.feeds.uncachedFeeds.comments.CommentFragment.Companion.COMMENT_STATUS_ID
 import org.pixeldroid.app.utils.BaseThemedWithBarActivity
 import org.pixeldroid.app.utils.api.PixelfedAPI
 import org.pixeldroid.app.utils.api.objects.Status
@@ -30,18 +18,13 @@ import org.pixeldroid.app.utils.api.objects.Status.Companion.POST_COMMENT_TAG
 import org.pixeldroid.app.utils.api.objects.Status.Companion.POST_TAG
 import org.pixeldroid.app.utils.api.objects.Status.Companion.VIEW_COMMENTS_TAG
 import org.pixeldroid.app.utils.displayDimensionsInPx
-import org.pixeldroid.app.utils.setProfileImageFromURL
 import retrofit2.HttpException
 import java.io.IOException
 
 class PostActivity : BaseThemedWithBarActivity() {
-    lateinit var domain : String
-
     private lateinit var binding: ActivityPostBinding
-    private lateinit var profileAdapter: PagingDataAdapter<Status, RecyclerView.ViewHolder>
-    private lateinit var commentViewModel: FeedViewModel<Status>
-    private var job: Job? = null
 
+    private var commentFragment = CommentFragment()
 
     private lateinit var status: Status
 
@@ -58,8 +41,6 @@ class PostActivity : BaseThemedWithBarActivity() {
 
         val user = db.userDao().getActiveUser()
 
-        domain = user?.instance_uri.orEmpty()
-
         supportActionBar?.title = getString(R.string.post_title).format(status.account?.getDisplayName())
 
         val holder = StatusViewHolder(binding.postFragmentSingle)
@@ -67,7 +48,7 @@ class PostActivity : BaseThemedWithBarActivity() {
         holder.bind(status, apiHolder, db, lifecycleScope, displayDimensionsInPx(), isActivity = true)
 
         activateCommenter()
-        retrieveComments()
+        initCommentsFragment(domain = user?.instance_uri.orEmpty())
 
         if(viewComments || postComment){
             //Scroll already down as much as possible (since comments are not loaded yet)
@@ -101,24 +82,15 @@ class PostActivity : BaseThemedWithBarActivity() {
         }
     }
 
-    @OptIn(ExperimentalPagingApi::class)
-    private fun retrieveComments() {
-            // get the view model
-            @Suppress("UNCHECKED_CAST")
-            commentViewModel = ViewModelProvider(this@PostActivity, ProfileViewModelFactory(
-                CommentContentRepository(
-                    apiHolder.setToCurrentUser(),
-                    status.id
-                )
-            )
-            )[FeedViewModel::class.java] as FeedViewModel<Status>
+    private fun initCommentsFragment(domain: String) {
 
-            profileAdapter = CommentAdapter()
-            initAdapter(binding.postCommentsProgressBar, binding.postRefreshLayout,
-                binding.commentRecyclerView, binding.motionLayout, binding.errorLayout,
-                profileAdapter)
+        val arguments = Bundle()
+        arguments.putSerializable(COMMENT_STATUS_ID, status.id)
+        arguments.putSerializable(COMMENT_DOMAIN, domain)
+        commentFragment.arguments = arguments
 
-            job = launch(job, lifecycleScope, commentViewModel, profileAdapter)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.commentFragment, commentFragment).commit()
     }
 
     private suspend fun postComment(
@@ -131,8 +103,8 @@ class PostActivity : BaseThemedWithBarActivity() {
                 val response = api.postStatus(nonNullText, it)
                 binding.commentIn.visibility = View.GONE
 
-                //Add the comment to the comment section
-                profileAdapter.refresh()
+                //Reload to add the comment to the comment section
+                commentFragment.adapter.refresh()
 
                 Toast.makeText(
                     binding.root.context,
@@ -153,83 +125,5 @@ class PostActivity : BaseThemedWithBarActivity() {
                 Log.e("ERROR_CODE", exception.code().toString())
             }
         }
-    }
-
-
-    inner class CommentViewHolder(val binding: CommentBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun bind(comment: Status) {
-
-            setProfileImageFromURL(binding.profilePic,
-                comment.account!!.anyAvatar(),
-                binding.profilePic
-            )
-            binding.user.text = comment.account.username
-            binding.commentText.text = parseHTMLText(
-                comment.content!!,
-                comment.mentions.orEmpty(),
-                apiHolder,
-                this@PostActivity,
-                lifecycleScope
-            )
-
-            binding.postDomain.text = comment.getStatusDomain(domain, binding.postDomain.context)
-
-            if(comment.replies_count == 0 || comment.replies_count == null){
-                binding.replies.visibility = View.GONE
-            } else {
-                binding.replies.visibility = View.VISIBLE
-                binding.replies.text = resources.getQuantityString(
-                    R.plurals.replies_count,
-                    comment.replies_count,
-                    comment.replies_count
-                )
-            }
-
-            binding.comment.setOnClickListener{ openComment(comment) }
-            binding.profilePic.setOnClickListener{ comment.account.openProfile(itemView.context) }
-            binding.user.setOnClickListener { comment.account.openProfile(itemView.context) }
-        }
-
-        private fun openComment(comment: Status) {
-            val intent = Intent(itemView.context, PostActivity::class.java).apply {
-                putExtra(POST_TAG, comment)
-            }
-            itemView.context.startActivity(intent)
-        }
-    }
-
-
-    inner class CommentAdapter : PagingDataAdapter<Status, RecyclerView.ViewHolder>(
-        UIMODEL_COMPARATOR
-    ) {
-        fun create(parent: ViewGroup): CommentViewHolder {
-            val itemBinding = CommentBinding.inflate(
-                LayoutInflater.from(parent.context), parent, false
-            )
-            return CommentViewHolder(itemBinding)
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            return create(parent)
-        }
-
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            val post = getItem(position)
-
-            post?.let {
-                (holder as CommentViewHolder).bind(it)
-            }
-        }
-        }
-
-
-    private val UIMODEL_COMPARATOR = object : DiffUtil.ItemCallback<Status>() {
-        override fun areItemsTheSame(oldItem: Status, newItem: Status): Boolean {
-            return oldItem.id == newItem.id
-        }
-
-        override fun areContentsTheSame(oldItem: Status, newItem: Status): Boolean =
-            oldItem.content == newItem.content
     }
 }
