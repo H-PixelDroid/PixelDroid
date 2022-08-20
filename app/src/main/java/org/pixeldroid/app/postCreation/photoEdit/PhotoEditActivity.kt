370efe184b83e275df2b247699c4c1f668b6aa61
Matthieu <24-artectrex@users.noreply.shinice.net>
2022-08-21 00:40:15 +0200

    Update dependencies

--- app/src/main/java/org/pixeldroid/app/postCreation/photoEdit/PhotoEditActivity.kt
+++ app/src/main/java/org/pixeldroid/app/postCreation/photoEdit/PhotoEditActivity.kt
@@ -296,6 +296,7 @@ class PhotoEditActivity : BaseThemedWithBarActivity() {
         uCrop.start(this)
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(resultCode == Activity.RESULT_OK) {
